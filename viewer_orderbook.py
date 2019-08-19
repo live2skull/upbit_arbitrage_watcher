c63495f4a1b613f65ce28882f583ca88a1a04efd
live2skull <live2skull@gmail.com>
2019-08-19 14:35:44 +0900

    add viewer_orderbook:

--- viewer_orderbook.py
+++ viewer_orderbook.py
@@ -1,16 +1,22 @@
 from sys import argv
-import signal, os
+import signal, os, sys
 
 from dotenv import load_dotenv
 from dunamu import orderbook, misc
 
-
+from threading import Thread, RLock
+from time import sleep
 
 import pika
 from pika.adapters.blocking_connection import BlockingChannel
 
 # https://www.rabbitmq.com/tutorials/tutorial-five-python.html
 # https://pika.readthedocs.io/en/stable/modules/channel.html
+
+lock = RLock()
+counter = {}
+
+clear = 'cls' if os.name == 'nt' else 'clear'
 
 def main():
     conn = misc.create_pika_connection()
@@ -27,41 +33,58 @@ def main():
     # 여러개를 바인딩할수 있습니다.
     channel.queue_bind(exchange='orderbook', queue=queue_name, routing_key="*")
 
-    print(' [*] Waiting for logs. To exit press CTRL+C')
 
     def callback(ch, method, properties, body):
-        print(method) # all about current message!
-        # Basic.Deliver ->
-        # <Basic.Deliver(['consumer_tag=ctag1.b491f0f260724892867fcc0e338d186e', 'delivery_tag=1', 'exchange=orderbook', 'redelive
-        # red=False', 'routing_key=KRW-BTC'])>
-        # method.routing_key
-        print(" [x] %r:%r" % (method.routing_key, body))
+
+        market = method.routing_key
+
+        with lock:
+            counter.setdefault(market, 0)
+            counter[market] += 1
+
+    def monitor():
+        while True:
+
+            os.system(clear)
+
+            with lock:
+
+                all = 0
+                bases = {}
+                bases.setdefault('KRW', 0)
+                bases.setdefault('BTC', 0)
+                bases.setdefault('ETH', 0)
+                bases.setdefault('USDT', 0)
+
+                for key in counter.items():
+                    base = key[0].split('-')[0]
+                    bases[base] += 1
+                    all += 1
+
+                print("Total= %s" % all)
+                for key, value in bases.items():
+                    sys.stdout.write("%s= %s " % (key, value))
+                print("\n")
+
+
+                for key, value in counter.items():
+                    print("%10s -> %02d" % (key, value))
+
+                counter.clear()
+            sleep(1)
+
 
     channel.basic_consume(
-        queue=queue_name, on_message_callback=callback)
+        queue=queue_name, on_message_callback=callback
+    )
+
+    thread = Thread(target=monitor)
+    thread.start()
 
     channel.start_consuming()
 
 
 
-def send():
-    conn = misc.create_pika_connection()
-    channel = conn.channel()  # type: BlockingChannel
-
-    channel.exchange_declare('orderbook', exchange_type='topic')
-
-    routing_key = 'KRW-BTC'
-    message = 'updated!'
-    channel.basic_publish(
-        exchange='orderbook', routing_key=routing_key, body=message.encode(),
-        properties=pika.spec.BasicProperties(delivery_mode=1)
-    )
-    print(" [x] Sent %r:%r" % (routing_key, message))
-    conn.close()
-
-
 if __name__ == '__main__':
     load_dotenv()
-
-    if len(argv) is 1: main()
-    else: send()
+    main()
